0856461bf430cabca4127ff6013ac2e3b97943d5
raul canul <eduardoct120@gmail.com>
2025-11-08 22:06:03 -0600

    @feature: libs shared implemented

--- zenithpass/apps/register-service/src/app/app.module.ts
+++ zenithpass/apps/register-service/src/app/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL || 'localhost',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
