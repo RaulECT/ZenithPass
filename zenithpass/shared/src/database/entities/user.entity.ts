e4232e016a3e66072de6b3c77ca2e8575e9d80ee
raul canul <eduardoct120@gmail.com>
2025-11-08 22:31:21 -0600

    @feature: user entity created

---
+++ zenithpass/shared/src/database/entities/user.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm'
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  fullName: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  passwordHash: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
